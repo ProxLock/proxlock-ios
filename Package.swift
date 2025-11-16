650de6f7aa70df1c139ae44f5b7e1f01a9b9e8ae
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-11-15 21:43:16 -0800

    name fixes

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "proxlock-ios",
+    name: "ProxLock",
     platforms: [.iOS(.v13), .macOS(.v10_15), .watchOS(.v6), .visionOS(.v1), .tvOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "proxlock-ios",
-            targets: ["proxlock-ios"]
+            name: "ProxLock",
+            targets: ["ProxLock"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "proxlock-ios"
+            name: "ProxLock"
         ),
     ]
 )
